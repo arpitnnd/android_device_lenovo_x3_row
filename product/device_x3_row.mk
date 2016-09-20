7a5a0a72dc53b064fd214a4dfaa48193efceb71c
Arpit Anand <arpit_nnd@outlook.com>
2016-09-20 12:18:29 +0530

    wifi: Add missing WCNSS_cfg.dat and use some more prebuilt files.

--- product/device_x3_row.mk
+++ product/device_x3_row.mk
@@ -329,17 +329,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/wifi/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
 	$(LOCAL_PATH)/wifi/hostapd.accept:system/etc/hostapd/hostapd.accept \
-	$(LOCAL_PATH)/wifi/hostapd.deny:system/etc/hostapd/hostapd.deny
-	#$(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+	$(LOCAL_PATH)/wifi/hostapd.deny:system/etc/hostapd/hostapd.deny \
+	$(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
 	$(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
-#PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat \
-	$(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/wifi/WCNSS_cfg.dat \
+	$(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
 PRODUCT_PACKAGES += \
-	p2p_supplicant_overlay.conf \
-	wpa_supplicant_overlay.conf \
 	wpa_supplicant.conf \
 	wpa_supplicant \
 	libwpa_client \
