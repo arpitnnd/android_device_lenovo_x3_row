50554756ab9f69b589c48cb28776f0b364457a4d
Arpit Anand <arpit_nnd@outlook.com>
2017-01-09 13:25:22 +0530

    init is now cpp

--- init/init_x3_row.cpp
+++ init/init_x3_row.cpp
@@ -1,5 +1,6 @@
 /*
    Copyright (c) 2016, The CyanogenMod Project
+
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -12,6 +13,7 @@
     * Neither the name of The Linux Foundation nor the names of its
       contributors may be used to endorse or promote products derived
       from this software without specific prior written permission.
+
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
@@ -25,46 +27,35 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <cstdio>
 
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
 
-static int read_file2(const char *fname, char *data, int max_size)
+static int read_file2(const char *fname, char *data)
 {
-    int fd, rc;
-
-    if (max_size < 1)
-        return 0;
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
+    FILE * file = std::fopen(fname, "r");
+    if (file == NULL) {
         ERROR("failed to open '%s'\n", fname);
         return 0;
     }
 
-    rc = read(fd, data, max_size - 1);
-    if ((rc > 0) && (rc < max_size))
-        data[rc] = '\0';
-    else
+    std::size_t rc = std::fread(data, 1, 1, file);
+    if (rc != 1)
         data[0] = '\0';
-    close(fd);
 
+    std::fclose(file);
     return 1;
 }
 
 static void init_alarm_boot_properties()
 {
     char const *alarm_file = "/proc/sys/kernel/boot_reason";
-    char buf[64];
+    char buf[1];
 
-    if (read_file2(alarm_file, buf, sizeof(buf))) {
+    if (read_file2(alarm_file, buf)) {
         /*
          * Setup ro.alarm_boot value to true when it is RTC triggered boot up
          * For existing PMIC chips, the following mapping applies
@@ -89,5 +80,5 @@ static void init_alarm_boot_properties()
 
 void vendor_load_properties()
 {
-    init_alarm_boot_properties();
+  init_alarm_boot_properties();
 }
