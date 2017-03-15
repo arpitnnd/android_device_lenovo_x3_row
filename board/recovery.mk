cb5b159c1caacfd3136567da7435f3677cefcfe8
Arpit Anand <arpit_nnd@outlook.com>
2017-03-15 19:46:18 +0530

    Add TWRP support
    To build:
    * Clone https://github.com/LineageOS/android_external_busybox to external/busybox
    * Clone https://github.com/omnirom/android_bootable_recovery/ to bootable/recovery-twrp
    * Set WITH_TWRP=true & do mka recoveryimage

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,11 +1,23 @@
-# Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
+USE_TWRP := false
 
-#RECOVERY_VARIANT := twrp
-#TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/twrp.fstab
+ifeq ($(USE_TWRP),true)
+#TWRP (optional)
+RECOVERY_VARIANT := twrp
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/twrp.fstab
 
-# TWRP
+BOARD_HAS_NO_REAL_SDCARD := true
+DEVICE_RESOLUTION := 1080x1920
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_THEME := portrait_hdpi
 TW_SCREEN_BLANK_ON_BOOT := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+
 TW_INCLUDE_CRYPTO := true
+else
+	TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
+endif
